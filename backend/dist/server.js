604f4ff4cd50d174a1b7487c4e99a2207ea862c2
Luis Felipe <luisfsill@hotmail.com>
2024-12-02 20:56:06 -0300

    update

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,23 +1,26 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const fastify_1 = __importDefault(require("fastify"));
-const routes_1 = require("./routes");
-const cors_1 = __importDefault(require("@fastify/cors"));
-const app = (0, fastify_1.default)({ logger: true });
+import fastify from 'fastify';
+import { routes } from './routes';
+import cors from '@fastify/cors';
+
+const app = fastify({ logger: true });
+
 app.setErrorHandler((error, request, reply) => {
-    reply.code(400).send({ messase: error.message });
+  reply.code(400).send({ message: error.message });
 });
+
 const start = async () => {
-    await app.register(cors_1.default);
-    await app.register(routes_1.routes);
-    try {
-        await app.listen({ port: 8080 });
-    }
-    catch (err) {
-        process.exit(1);
-    }
+  await app.register(cors);
+  await app.register(routes);
+
+  try {
+    await app.listen({ 
+      port: parseInt(process.env.PORT || '8080'), 
+      host: '0.0.0.0' 
+    });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
 };
+
 start();
